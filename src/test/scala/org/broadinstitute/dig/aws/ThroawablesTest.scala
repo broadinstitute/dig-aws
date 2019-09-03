b25433995562c2094dda656a648689c565f10804
clint <cgilbert@broadinstitute.org>
2019-09-03 15:17:18 -0400

    Added ThrowablesTest

---
+++ src/test/scala/org/broadinstitute/dig/aws/ThroawablesTest.scala
@@ -0,0 +1,38 @@
+package org.broadinstitute.dig.aws
+
+import org.scalatest.FunSuite
+
+
+/**
+ * @author clint
+ * Sep 3, 2019
+ */
+final class ThroawablesTest extends FunSuite {
+  test("quietly") {
+    import Throwables.quietly
+    
+    val dontLog: (String, Throwable) => Unit = (_, _) => ()
+    
+    assert(quietly("foo", dontLog)(42) === None)
+    
+    var logFnParams: Seq[(String, Throwable)] = Vector.empty
+    
+    val myLogFn: (String, Throwable) => Unit = (msg, t) => {
+      logFnParams :+= (msg -> t)
+      
+      ()
+    }
+    
+    assert(logFnParams.isEmpty)
+    
+    assert(quietly("foo", myLogFn)(42) === None)
+    
+    assert(logFnParams.isEmpty)
+    
+    val e = new Exception with scala.util.control.NoStackTrace
+    
+    assert(quietly("blarg", myLogFn)(throw e) === Some(e))
+    
+    assert(logFnParams === Seq(("blarg", e)))
+  }
+}
